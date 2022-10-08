83d3f928ccbe2b8e1fae91e5c3781878e3df1ee6
levvw <abdelraahmanmostafa@gmail.com>
2022-10-08 13:28:09 +0200

    changed start command to use ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
-
+const port = process.env.PORT || 3000;
 import connectToMongoDB from "./connections/mongo";
 connectToMongoDB("mongodb://127.0.0.1:27017", "DatabaseName");
 
 app.listen(process.env.PORT, () => {
-  console.log(`server started on port ${process.env.PORT}`);
+  console.log(`server started on port ${port}`);
 });
