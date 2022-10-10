a90d33e136fb66d82352e79f4c4b3beff4b33676
levvw <abdelraahmanmostafa@gmail.com>
2022-10-10 20:02:54 +0200

    👩🏽‍💻 changing connect method to take 1 param

--- src/connections/mongo.ts
+++ src/connections/mongo.ts
@@ -1,9 +1,12 @@
 import { MongoClient, Db } from "mongodb";
 
-const connectToMongoDB = async (connectionURL: string, DbName: string) => {
+const connectToMongoDB = async (connection: string) => {
+  const dashIndex = connection.indexOf("/", 11); // 11 to skip the / in "mongodb://"
+  const connectionURL = connection.slice(0, dashIndex);
+  const DBName = connection.slice(dashIndex + 1);
   const client: MongoClient = new MongoClient(connectionURL);
   await client.connect();
-  const db: Db = client.db(DbName);
+  const db: Db = client.db(DBName);
   console.log(`Connected to MongoDB: ${db.databaseName}`);
 };
 
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 import connectToMongoDB from "./connections/mongo";
-connectToMongoDB("mongodb://127.0.0.1:27017", "DatabaseName");
+connectToMongoDB("mongodb://127.0.0.1:27017/DatabaseName");
 
 app.listen(process.env.PORT, () => {
   console.log(`server started on port ${port}`);
